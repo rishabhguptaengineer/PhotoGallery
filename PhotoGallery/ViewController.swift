9351dc72a41698c334a5715d6ef52ef6ba36788e
Rishabh Gupta <rishabhgupta.engineer@gmail.com>
2026-06-13 19:50:52 +0530

    Initial Commit

---
+++ PhotoGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoGallery
+//
+//  Created by Rishabh Gupta on 13-06-2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
